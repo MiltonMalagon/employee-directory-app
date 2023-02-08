cba9ec503078b8e1286cffe5423a8bc4de760fd9
Milton Malagón <83915699+MiltonMalagon@users.noreply.github.com>
2023-02-08 14:12:02 -0500

    Add gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//--- SEARCH CONTAINER ---//
+//--- SEARCH BAR ---//
 const searchContainer = document.querySelector(".search-container");
 const searchHTML = `
   <form action="#" method="get">
@@ -6,5 +6,22 @@ const searchHTML = `
     <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
   </form>
 `;
+//--- GALLERY ---//
+const galleryContainer = document.querySelector("#gallery");
+const galleryHTML = `
+  <div class="card">
+      <div class="card-img-container">
+          <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+      </div>
+      <div class="card-info-container">
+          <h3 id="name" class="card-name cap">first last</h3>
+          <p class="card-text">email</p>
+          <p class="card-text cap">city, state</p>
+      </div>
+  </div>
+`;
+
 
 searchContainer.insertAdjacentHTML("beforeend", searchHTML);
+galleryContainer.insertAdjacentHTML("beforeend", galleryHTML);
+
