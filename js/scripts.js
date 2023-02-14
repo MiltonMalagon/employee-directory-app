1f1727fbf3cabf9b6c231772aaaab45292be81d6
Milton Malagón <83915699+MiltonMalagon@users.noreply.github.com>
2023-02-13 19:08:47 -0500

    Fetches data (refactored)

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,94 @@
-//--- SEARCH BAR ---//
-const searchContainer = document.querySelector(".search-container");
-const searchHTML = `
-  <form action="#" method="get">
-    <input type="search" id="search-input" class="search-input" placeholder="Search...">
-    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-  </form>
-`;
-//--- GALLERY ---//
-const galleryContainer = document.querySelector("#gallery");
-const galleryHTML = `
-  <div class="card">
-      <div class="card-img-container">
-          <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-      </div>
-      <div class="card-info-container">
-          <h3 id="name" class="card-name cap">first last</h3>
-          <p class="card-text">email</p>
-          <p class="card-text cap">city, state</p>
-      </div>
-  </div>
-`;
-//--- MODAL ---//
-const modalHTML = `
-  <div class="modal-container">
-      <div class="modal">
-          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-          <div class="modal-info-container">
-              <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-              <h3 id="name" class="modal-name cap">name</h3>
-              <p class="modal-text">email</p>
-              <p class="modal-text cap">city</p>
-              <hr>
-              <p class="modal-text">(555) 555-5555</p>
-              <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-              <p class="modal-text">Birthday: 10/21/2015</p>
-          </div>
-      </div>
+document.addEventListener("DOMContentLoaded", async (e) => {
+  //--- SEARCH BAR ---//
+  const searchContainer = document.querySelector(".search-container");
+  const searchHTML = `
+    <form action="#" method="get">
+      <input type="search" id="search-input" class="search-input" placeholder="Search...">
+      <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>
+  `;
+  //--- GALLERY ---//
+  const galleryContainer = document.querySelector("#gallery");
+  const galleryHTML = `
+    <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">first last</h3>
+            <p class="card-text">email</p>
+            <p class="card-text cap">city, state</p>
+        </div>
+    </div>
+  `;
+  //--- MODAL ---//
+  const modalHTML = `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                <h3 id="name" class="modal-name cap">name</h3>
+                <p class="modal-text">email</p>
+                <p class="modal-text cap">city</p>
+                <hr>
+                <p class="modal-text">(555) 555-5555</p>
+                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+        </div>
 
-      // IMPORTANT: Below is only for exceeds tasks 
-      <div class="modal-btn-container">
-          <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-          <button type="button" id="modal-next" class="modal-next btn">Next</button>
-      </div>
-  </div>
-`;
+        // IMPORTANT: Below is only for exceeds tasks 
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>
+  `;
 
-searchContainer.insertAdjacentHTML("beforeend", searchHTML);
-galleryContainer.insertAdjacentHTML("beforeend", galleryHTML);
+  searchContainer.insertAdjacentHTML("beforeend", searchHTML);
+  galleryContainer.insertAdjacentHTML("beforeend", galleryHTML);
 
-function checkStatus(response) {
-  if (response.ok) {
-    return Promise.resolve(response);
-  } else {
-    return Promise.reject(new Error(response.statusText))
+  //--- HELPER FUNCTION ---//
+  // function checkStatus(response) {
+  //   if (response.ok && response.status === 200) {
+  //     return Promise.resolve(response);
+  //   } else {
+  //     return Promise.reject(new Error(response.statusText))
+  //   }
+  // }
+
+  //--- REQUEST HANDLER ---//
+  async function fetchData(url) {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      throw error;
+    }
   }
-}
 
-function fetchData(url) {
-  return fetch(url)
-    .then(checkStatus)
-    .then(response => response.json())
-    .catch(error => console.log(error));
-}
+  //--- HTML GENERATOR ---//
 
-Promise.all([
-  fetchData("https://randomuser.me/api/")
-    .then(data => console.log(data))
-]);
+
+  //--- MAKE REQUEST ---//
+  try {
+    const data = await fetchData("https://randomuser.me/api/?results=24");
+    console.log(data);
+  } catch (error) {
+    throw error;
+  }
+
+  // function fetchData(url) {
+  //   return fetch(url)
+  //     .then(checkStatus)
+  //     .then(response => response.json())
+  //     .catch(error => console.log(error));
+  // }
+
+  // Promise.all([
+  //   fetchData("https://randomuser.me/api/")
+  // ])
+  //   .then(data => console.log(data));
+});
